4c9e35c76f326b4c5a2dafa297cdac91496d85ad
David Sun <ffstudios1@gmail.com>
2016-05-26 09:57:59 -0700

    Testing Date.now()

--- js/index.js
+++ js/index.js
@@ -7,14 +7,17 @@ window.onload = function(e){
     document.getElementById("timer").textContent=start.toFixed(1)+"s"
   },100);
   dbRef.limitToLast(1).on('child_added', function(data) {
+    console.log(data.val().rageTime);
+    console.log("now",data);
     start = Math.abs(Date.now() - data.val().rageTime);
+    console.log(start);
   });
   document.getElementById("rage").onclick = function() {
     start = 0;
-    alert("He raged! Hallelujah! Tell all who is playing the alex rage drinking game to start drinking!")
+    alert("He raged! Hallelujah! Tell all who are playing the alex rage drinking game to start drinking!")
     rageCounter++;
     document.getElementById("rage-counter").textContent="Rage Counter: "+rageCounter;
-    dbRef.push({rageTime:Date.now()});
+    dbRef.push({rageTime:firebase.database.ServerValue.TIMESTAMP});
   }
   document.getElementById("rage-counter").textContent="Rage Counter: "+rageCounter;
 }
